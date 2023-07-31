b767f0fe47d92238ecdd01a83f1c6172fd847d41
ozzygotnodrip <orestis.mangoyan@gmail.com>
2023-07-31 20:35:03 +0300

    no center of mass code

--- Astro project/Assets/PlayerMovement.cs
+++ Astro project/Assets/PlayerMovement.cs
@@ -2,18 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(RigidBody))]
-public class CenterOfMass : MonoBehaviour
 public class PlayerControls : MonoBehaviour
 {
-    public Vector3 CenterOfMass2;
-    public bool Awake;
-    protected Rigidbody r;
+
     
-    void Start()
-    {
-        r = GetComponent<Rigidbody>();
-    }
 
     public float speed = 10.0f;
     void FixedUpdate()
@@ -30,16 +22,9 @@ public class PlayerControls : MonoBehaviour
         float angle = Mathf.Atan2(lookPos.y, lookPos.x) * Mathf.Rad2Deg;
         transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
 
-        //code to change center of mass on character
-        r.centerOfMass - CenterOfMass2;
-        r.WakeUp();
-        Awake - !r.IsSleeping();
+      
     }
 
-    private void OnDrawGizmos()
-    {
-        Gizmos.color = Color.red;
-        Gizmos.DrawSphere(transform.position + transform.rotation * CenterOfMass2, 1f);
-    }
+  
 } 
 
